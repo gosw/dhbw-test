d855b009be7d905892845444b964cff48b890071
nbollmann <nico.bollmann94@gmail.com>
2016-09-11 09:53:25 +0200

    Fibonacci rekursiv implementiert, diverse Zahlen getestet

--- src/main/java/de/dhbw/Fibonacci.java
+++ src/main/java/de/dhbw/Fibonacci.java
@@ -1,10 +1,11 @@
 package de.dhbw;
 
 public class Fibonacci {
-    public static int getFib(int n)
+    public static int getFib(final int n)
     {
-        if (n == 1)
-            return 1;
-        return getFib(n-2)+ getFib(n-1);
+        if (n <= 2) {
+            return (n > 0) ? 1 : 0;
+        }
+        return getFib(n - 1) + getFib(n - 2);
     }
 }
