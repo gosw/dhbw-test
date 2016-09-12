2c742d87424a1335c73a1f5366c2840fe068a78c
nbollmann <nico.bollmann94@gmail.com>
2016-09-12 19:16:59 +0200

    QuickSort und Test implementiert

--- src/main/java/de/dhbw/QuickSort.java
+++ src/main/java/de/dhbw/QuickSort.java
@@ -1,12 +1,41 @@
 package de.dhbw;
 
-import java.util.Vector;
-
 public class QuickSort {
 
     public static int[] sortieren(int[] liste){
-        //TODO: Implementierung
+        qSort(liste, 0, liste.length - 1);
         return liste;
     }
 
+    private static void qSort(int[] list, int left, int right){
+        if (left < right) {
+            int i = partition(list, left, right);
+            qSort(list, left, i-1);
+            qSort(list, i + 1, right);
+        }
+    }
+
+    private static int partition(int[] list, int left, int right){
+        int pivot = list[right];
+        int i = left;
+        int j = right - 1;
+        int help;
+        while (i <= j){
+            if (list[i] > pivot){
+                //tausche list[i] und list[j]
+                help = list[i];
+                list[i] = list[j];
+                list[j] = help;
+                j--;
+            } else {
+                i++;
+            }
+        }
+        // tausche x[i] und x[rechts]
+        help = list[i];
+        list[i] = list[right];
+        list[right] = help;
+
+        return i;
+    }
 }
---
+++ src/test/java/de/dhbw/QuickSortTest.java
@@ -0,0 +1,15 @@
+package de.dhbw;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class QuickSortTest {
+
+    @Test
+    public void testSortieren() throws Exception {
+        int[] liste = new int[]{0, 8, 6, 7 , 1, 9, 3, 2, 5, 4};
+        int[] correctListe = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        assertArrayEquals(correctListe, QuickSort.sortieren(liste));
+    }
+}
