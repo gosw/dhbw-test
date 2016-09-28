f3a858bee2d65a529ffad100fddc177bf38ef5cf
Tim <tiga05@gmx.de>
2016-09-28 13:37:21 +0200

    Klassennamen angepasst - nach entsprechenden Vorlagen

--- src/test/java/de/dhbw/randomGeneratorTest.java
+++ src/test/java/de/dhbw/randomGeneratorTest.java
@@ -6,11 +6,11 @@ import static org.junit.Assert.assertEquals;
 /**
  * Created by GAUDICHT on 28.09.2016.
  */
-public class randomGeneratorTest{
+public class RandomGeneratorTest {
     @Test
     public void testGenerator() throws Exception {
         int anzahlZahlen=(int) Math.random()*10;
-        assertEquals(anzahlZahlen,randomGenerator.randomZahlen(anzahlZahlen).length);
+        assertEquals(anzahlZahlen, RandomGenerator.randomZahlen(anzahlZahlen).length);
 
     }
 
